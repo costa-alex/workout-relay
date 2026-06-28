9402395c153bf84d95898f145ad512e0bd904fcb
Alexandre Costa <costa.alex@gmail.com>
2026-06-28 10:49:15 +0100

    feat: implement responsive drawer layout with navigation and version display

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,24 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from "app/top-bar/top-bar.component";
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { forkJoin } from 'rxjs';
+import * as semver from 'semver';
 
+import { TopBarComponent } from 'app/top-bar/top-bar.component';
+import { EnvironmentService } from 'infrastructure/environment.service';
+import { GitHubClient } from 'infrastructure/client/github.client';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TopBarComponent
+    RouterLink,
+    TopBarComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
+  appVersion = '';
+  updateAvailableBadgeHidden = true;
+  githubLink = 'https://github.com/costa-alex/tp2intervals';
+
+  menuButtons = [
+    { icon: 'home', name: 'Home', url: '/home' },
+    { icon: 'timeline', name: 'TrainingPeaks', url: '/training-peaks' },
+    { icon: 'directions_bike', name: 'TrainerRoad', url: '/trainer-road' },
+    { icon: 'settings', name: 'Configuration', url: '/config' }
+  ];
+
   constructor(
+    protected router: Router,
+    private githubClient: GitHubClient,
+    private environmentService: EnvironmentService
   ) {
   }
 
   ngOnInit(): void {
+    forkJoin([
+      this.githubClient.getLatestRelease(),
+      this.environmentService.getVersion()
+    ]).subscribe(([latestRelease, appVersion]) => {
+      this.appVersion = appVersion;
+
+      if (semver.gt(latestRelease.version, this.appVersion)) {
+        this.updateAvailableBadgeHidden = false;
+        this.githubLink = latestRelease.url;
+      }
+    });
   }
 }
--- ui/src/app/top-bar/top-bar.component.ts
+++ ui/src/app/top-bar/top-bar.component.ts
@@ -1,112 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {EnvironmentService} from "infrastructure/environment.service";
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatBadgeModule} from "@angular/material/badge";
-import {forkJoin} from "rxjs";
-import {GitHubClient} from "infrastructure/client/github.client";
-import * as semver from "semver";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { map, shareReplay } from 'rxjs/operators';
-import { MatListModule } from '@angular/material/list';
-import { MatDividerModule } from '@angular/material/divider';
-import { ViewChild } from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
-import { NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [
-    MatButtonModule,
-    MatToolbarModule,
+    AsyncPipe,
     RouterLink,
     MatBadgeModule,
-    MatTooltipModule,
+    MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    MatDividerModule,
-    AsyncPipe
+    MatToolbarModule,
+    MatTooltipModule
   ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
+export class TopBarComponent {
 
-export class TopBarComponent implements OnInit {
-  @ViewChild(MatDrawer)
-  drawer?: MatDrawer;
+  @Input() menuButtons: { icon: string; name: string; url: string }[] = [];
+  @Input() appVersion = '';
+  @Input() githubLink = 'https://github.com/costa-alex/tp2intervals';
+  @Input() updateAvailableBadgeHidden = true;
 
-  appVersion: string
-  updateAvailableBadgeHidden = true;
-  githubLink = 'https://github.com/costa-alex/tp2intervals'
+  @Output() menuClicked = new EventEmitter<void>();
 
-  menuButtons = [
-    {
-      icon: 'home',
-      name: 'Home',
-      url: '/home'
-    },
-    {
-      icon: 'timeline',
-      name: 'TrainingPeaks',
-      url: '/training-peaks'
-    },
-    {
-      icon: 'directions_bike',
-      name: 'TrainerRoad',
-      url: '/trainer-road'
-    },
-    {
-      icon: 'settings',
-      name: 'Configuration',
-      url: '/config'
-    }
-  ];
+  readonly isMobile$ = this.breakpointObserver
+    .observe('(max-width: 768px)')
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
   constructor(
     protected router: Router,
-    private githubClient: GitHubClient,
-    private environmentService: EnvironmentService,
     private breakpointObserver: BreakpointObserver
   ) {
   }
-
-  readonly isMobile$ = this.breakpointObserver.observe('(max-width: 768px)')
-  .pipe(
-    map(result => result.matches),
-    shareReplay()
-  );
-
-  ngOnInit(): void {
-
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.drawer?.close();
-      });
-
-    forkJoin([
-      this.githubClient.getLatestRelease(),
-      this.environmentService.getVersion()
-    ]).subscribe(result => {
-
-      this.appVersion = result[1];
-      const latestRelease = result[0];
-
-      if (semver.gt(latestRelease.version, this.appVersion)) {
-        this.updateAvailableBadgeHidden = false;
-        this.githubLink = latestRelease.url;
-      }
-      console.log(result);
-    });
-
-  }
 }
-
