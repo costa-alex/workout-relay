58deaeb451a015b0f73467ba08b9b50dceaa4387
Alexandre Costam <costa.alex@gmail.com>
2026-07-15 15:12:37 +0100

    Refactor repository name from TP2Intervals to Workout Relay across workflows, configurations, and documentation

--- boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/Signature.kt
+++ boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/Signature.kt
@@ -3,7 +3,7 @@ package org.freekode.tp2intervals.infrastructure
 class Signature {
     companion object {
         val description = """
-            Imported with tp2intervals (https://github.com/costa-alex/tp2intervals)
+            Imported with tp2intervals (https://github.com/costa-alex/workout-relay)
         """.trimIndent()
     }
 }
